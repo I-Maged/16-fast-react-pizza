0e39b95214993f8c6399475d190390c48918c9a7
I-Maged <magedhanafy@gmail.com>
2026-02-13 13:40:33 +0200

    styling cart & menu

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom"
 import LinkButton from "../../ui/LinkButton"
 import Button from "../../ui/Button"
+import CartItem from "./CartItem"
 
 const fakeCart = [
   {
@@ -27,16 +27,24 @@ const fakeCart = [
 ]
 const Cart = () => {
   const cart = fakeCart
-  console.log(cart)
 
   return (
-    <div>
+    <div className="px-3 py-4">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
-      <div>
-        <Button to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+
+      <ul className="mt-3 divide-y divide-stone-200 border-b border-stone-200">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+        <Button type="secondary">Clear Cart</Button>
       </div>
     </div>
   )
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
   const isLoading = navigation.state === "loading"
 
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+    <div className="grid h-dvh grid-rows-[auto_1fr_auto]">
       {/* {true && <Loader />} */}
       {isLoading && <Loader />}
 
