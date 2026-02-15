123ca20aa2cb31803b3f33c04c1cae5e2c0c7154
I-Maged <magedhanafy@gmail.com>
2026-02-15 05:54:34 +0200

    create async thunk

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,12 +1,13 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom"
 import { createOrder } from "../../services/apiRestaurant"
 import Button from "../../ui/Button"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice"
 import EmptyCart from "../cart/EmptyCart"
 import store from "../../store"
 import { formatCurrency } from "../../utils/helpers"
 import { useState } from "react"
+import { fetchAddress } from "../user/userSlice"
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -39,8 +40,18 @@ const isValidPhone = (str) =>
 ] */
 
 const CreateOrder = () => {
-  const username = useSelector((state) => state.user.username)
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user)
+
+  const isLoadingAddress = addressStatus === "loading"
+
   const navigation = useNavigation()
+  const dispatch = useDispatch()
   const isSubmitting = navigation.state === "submitting"
 
   const formErrors = useActionData()
@@ -49,7 +60,7 @@ const CreateOrder = () => {
   const cart = useSelector(getCart)
   const [withPriority, setWithPriority] = useState(false)
   const totalCartPrice = useSelector(getTotalCartPrice)
-  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0
+  // const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0
 
   if (!cart.length) return <EmptyCart />
 
@@ -81,16 +92,37 @@ const CreateOrder = () => {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
+              disabled={isLoadingAddress}
               type="text"
               name="address"
               required
               className="input w-full"
+              defaultValue={address}
             />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className="absolute top-1 right-1 z-50 md:top-1.25 md:right-1.25">
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault()
+                  dispatch(fetchAddress())
+                }}
+              >
+                Get position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -109,7 +141,16 @@ const CreateOrder = () => {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button type="primary" disabled={isSubmitting}>
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude
+                ? `${position.longitude},${position.latitude}`
+                : ""
+            }
+          />
+          <Button type="primary" disabled={isSubmitting || isLoadingAddress}>
             {isSubmitting
               ? "Placing Order..."
               : `Order now for ${formatCurrency(totalCartPrice)}`}
@@ -132,9 +173,10 @@ export async function action({ request }) {
 
   const errors = {}
 
-  if (!isValidPhone(order.phone))
+  if (!isValidPhone(order.phone)) {
     errors.phone =
       "please give us your correct phone number. We might need it to contact you."
+  }
 
   if (Object.keys(errors).length > 0) return errors
 
